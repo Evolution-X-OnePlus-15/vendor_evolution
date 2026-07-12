509f476744e9b64b307b0bdd9fdc1309d031eb36
Marco Veldhuizen <marco_veldhuizen@hotmail.com>
2026-07-13 00:01:20 +0200

    vendor: Introduce App Lock [4/4]
    Change-Id: Ie6d86e301efe14146c961b11bfccd3fa74a1562c

--- config/common.mk
+++ config/common.mk
@@ -274,6 +274,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES_DEBUG += \
     disable-overlays
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
